1a23247e73bf2aedc95683953fc2b8a6655d664f
emorales18 <98432186+emorales18@users.noreply.github.com>
2022-05-18 19:56:41 -0700

    initial commit

---
+++ lab_assignment2.py
@@ -0,0 +1,25 @@
+"""
+start
+get the numbers of sheets
+sheets / 5
+round answer to next number
+output the result to the user
+end
+"""
+def calculate(sheet):
+    answer = sheet / 5
+    rounded = round(answer, 1)
+    print("sheet is:", sheet)
+    print("The answer is:", answer)
+    print("rounded is:", rounded)
+    print("==================================")
+    return rounded
+
+output = calculate (10000)
+
+print("the return statement is:", output)
+
+
+
+
+
